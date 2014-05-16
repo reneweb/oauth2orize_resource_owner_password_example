86c6524c56137463c6916dbd2df8c5dd8f1c7d1f
OpenShift guest <stagnate@live.de>
2014-05-16 19:57:02 -0400

    Made changes in auth process

--- oauth.js
+++ oauth.js
@@ -9,38 +9,45 @@ var server = oauth2orize.createServer();
 
 //Resource owner password
 server.exchange(oauth2orize.exchange.password(function (client, username, password, scope, done) {
-    var token = utils.uid(256)
-    var refreshToken = utils.uid(256)
-    var tokenHash = crypto.createHash('sha1').update(token).digest('hex')
-    var refreshTokenHash = crypto.createHash('sha1').update(refreshToken).digest('hex')
+    passport.authenticate('userLocal', { session: false }, function(req, res) {
+        var token = utils.uid(256)
+        var refreshToken = utils.uid(256)
+        var tokenHash = crypto.createHash('sha1').update(token).digest('hex')
+        var refreshTokenHash = crypto.createHash('sha1').update(refreshToken).digest('hex')
+        
+        var expirationDate = new Date(new Date().getTime() + (3600 * 1000))
     
-    var expirationDate = new Date(new Date().getTime() + (3600 * 1000))
-
-    db.collection('accessTokens').save({token: tokenHash, refreshToken: refreshTokenHash, expirationDate: expirationDate, clientId: client._id, userId: username, scope: scope}, function (err) {
-        if (err) return done(err)
-        done(null, token, refreshToken, {expires_in: expirationDate})
+        db.collection('accessTokens').save({token: tokenHash, refreshToken: refreshTokenHash, expirationDate: expirationDate, clientId: client._id, userId: username, scope: scope}, function (err) {
+            if (err) return done(err)
+            done(null, token, refreshToken, {expires_in: expirationDate})
+        })
     })
 }))
 
 //Refresh Token
 server.exchange(oauth2orize.exchange.refreshToken(function (client, refreshToken, scope, done) {
-    var token = utils.uid(256)
-    
-    var tokenHash = crypto.createHash('sha1').update(token).digest('hex')
     var refreshTokenHash = crypto.createHash('sha1').update(refreshToken).digest('hex')
     
-    var expirationDate = new Date(new Date().getTime() + (3600 * 1000))
-
-    db.collection('accessTokens').update({refreshToken: refreshTokenHash}, {token: tokenHash, scope: scope, expirationDate: expirationDate}, function (err) {
+    db.collection('accessTokens').findOne({refreshToken: refreshTokenHash}, function (err, token) {
         if (err) return done(err)
-        done(null, token, refreshToken, {expires_in: expirationDate});
+        if (!token) return done(null, false)
+        if (client.username !== token.clientID) return done(null, false)
+        
+        var newAccessToken = utils.uid(256)
+        var accessTokenHash = crypto.createHash('sha1').update(newAccessToken).digest('hex')
+        
+        var expirationDate = new Date(new Date().getTime() + (3600 * 1000))
+    
+        db.collection('accessTokens').update({refreshToken: refreshTokenHash}, {token: accessTokenHash, scope: scope, expirationDate: expirationDate}, function (err) {
+            if (err) return done(err)
+            done(null, newAccessToken, refreshToken, {expires_in: expirationDate});
+        })
     })
 }))
 
 // token endpoint
 exports.token = [
     passport.authenticate(['clientBasic', 'clientPassword'], { session: false }),
-    passport.authenticate('userLocal', { session: false }),
     server.token(),
     server.errorHandler()
 ]
