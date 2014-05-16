f573a18f35918d15ed7497e94911c00416714e32
OpenShift guest <stagnate@live.de>
2014-05-16 10:48:09 -0400

    Created database configuration

---
+++ app.js
@@ -0,0 +1,26 @@
+//Module dependencies
+var express = require('express')
+    , http = require('http')
+    , passport = require('passport')
+    , util = require('util')
+    , bodyParser = require('body-parser')
+    , expressValidator = require('express-validator')
+    , auth = require("auth")
+    , oauth = require("oauth")
+    , registration = require("registration")
+
+// Express configuration
+var app = express()
+app.use(bodyParser())
+app.use(expressValidator())
+
+app.use(passport.initialize())
+
+app.post('/users', registration.registerUser)
+app.post('/oauth/token', oauth.token)
+app.get('/restricted', passport.authenticate('accessToken', { session: false }), function (req, res) {
+    res.send("Yay, you successfully accessed the resitricted resource!")
+})
+
+//Start
+http.createServer(app).listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0")
---
+++ db.js
@@ -0,0 +1,5 @@
+var mongojs = require('mongojs')
+
+var db = mongojs(process.env.IP + '/oauth2orize_resource_owner_password_example')
+
+exports.getDb = db
